c5a5ef726da78f90557a869e641bcfb9088af7d8
MadameIlkyway <132264036+MadameIlkyway@users.noreply.github.com>
2024-03-07 12:34:34 +0000

    ü23oK in 2 Fragen geteilt, Minijob-Abfrage eingefügt, Abfolge der Entgelte bzw. Abfragen geändert

--- SV-Berechnung.py
+++ SV-Berechnung.py
@@ -21,26 +21,47 @@ print()
 #if dictKKZ.get(kk) is None:
    # print("get mit None!!!")
 
-entgelt = float(input("Eingabe Entgelt: "))
+entgelt = float(input("Eingabe Entgelt (sv-pflichtig): "))
+
+if entgelt < 538:
+        gfb = input("Liegt eine geringfügige Beschäftigung (Minijob) vor? (j/n) ")
+        if gfb == "j":
+            rvfrei = input("Liegt eine Befreiung der Rentenversicherungspflicht vor? (j/n) ")
+            if rvfrei == "j":
+                netto = entgelt
+            else: 
+                rv = 3.6 * entgelt / 100
+                netto = entgelt - rv
+        print()        
+        print("Dein Nettobetrag liegt bei ", netto, "Euro.")
+        quit()
+
+
 kk = input("Eingabe Krankenkasse: ")
 while kk not in dictKKZ:
    print("Gibts nicht. Versuchs noch einmal.")
    kk = input("Eingabe Krankenkasse: ")
 
-kinderlos = input("Über 23 Jahre ohne Kind (j/n): ")
+kinder = input("Hast du ein oder mehr Kind(er)? (j/n) ")
+ü23 = input("Bist du älter als 23 Jahre? (j/n) ")
 print()
 kkzusatz = dictKKZ[kk]
 
-if kinderlos == "j":
-    PVZusatz = 0.6
+if kinder == "j":
+   PVZusatz = 0
 else:
-    PVZusatz = 0
+    if ü23 == "n":
+        PVZusatz = 0
+    else:     
+        PVZusatz = 0.6
+        print("Du musst einen Zusatzbeitrag zur Pflegeversicherung zahlen.")
+
 
 if entgelt > 538 and entgelt < 2000.01:
     print("Es liegt der Übergangsbereich vor. SV müssen mittels Faktorrechnung ermittelt werden.")
     BBGAN = round(1.351351351 * entgelt - 702.7027027, 2)
     print("Die geminderte Beitragsbemessungsgrundlage liegt bei: ", BBGAN, "Euro.")
-    kv = round((7.3 + kkzusatz) * BBGAN / 100, 2)
+    kv = round((7.3 + (kkzusatz / 2)) * BBGAN / 100, 2)
     pv = round((2.2 + PVZusatz) * BBGAN / 100, 2)
     rv = round(9.3 * BBGAN / 100, 2)
     av = round(1.3 * BBGAN / 100, 2)
@@ -52,12 +73,9 @@ if entgelt > 538 and entgelt < 2000.01:
     print("RV: ", rv, "Euro")
     print("AV: ", av, "Euro")
 else :
-    if entgelt < 520:
-        print("Entgelt unter 538 Euro. Es sind die Regeln für geringfügige Beschäftigung zu prüfen.")
-    else :
         print("Hier ist der Halbteilungsgrundsatz anzuwenden. Bemessungsgrundlage:", entgelt, "Euro.")
         print()
-        kv = round((14.6 + kkzusatz) * entgelt / 100 / 2, 2)
+        kv = round((7.3 + (kkzusatz / 2)) * entgelt / 100, 2)
         pv = round((2.2 + PVZusatz) * entgelt / 100, 2)
         rv = round(9.3 * entgelt / 100, 2)
         av = round(1.3 * entgelt / 100, 2)
@@ -72,11 +90,11 @@ print("-------------------------")
 print("Es geht noch weiter.")
 print("Du willst doch bestimmt wissen, wie dein Nettobetrag aussehen wird.")
 print()
-print("Deine Lohnsteuer kannst du hier berechnen lassen: https://www.bmf-steuerrechner.de/bl/bl2023_01/eingabeformbl2023_01.xhtml")
+print("Deine Lohnsteuer kannst du hier berechnen lassen: https://www.bmf-steuerrechner.de/bl/bl2024/eingabeformbl2024.xhtml")
 print("Der benötige Zusatzbeitrag der Krankenkasse lautet: ", kkzusatz)
 print()
 steuern = float(input("Steuern: "))
 netto = entgelt - steuern - sv
 print()
-print("Dein Nettobetrag lautet: ", netto, "Euro.")
+print("Dein Nettobetrag lautet: ", round(netto, 2), "Euro.")
 print()
